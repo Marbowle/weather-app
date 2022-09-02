87b19ab7b25c6f653b77483c19b3613982d7f4e4
Marbowle <mahomegol@gmail.com>
2022-09-02 16:17:53 +0200

    Fluent icon change

--- script.js
+++ script.js
@@ -82,5 +82,13 @@ function showTemperature(response) {
   let weatherDescription = response.data.weather[0].description;
   let codnition = document.querySelector("#condition");
   codnition.innerHTML = `${weatherDescription}`;
+  let codnition = document.querySelector("#condition");
+  codnition.innerHTML = `${weatherDescription}`;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 search("Stockholm");
