00a5ffc916d063cfeb4d655524241699a4eb4a9b
Marbowle <mahomegol@gmail.com>
2022-09-22 11:19:27 +0200

    Display forecast

--- script.js
+++ script.js
@@ -37,6 +37,21 @@ let months = [
 ];
 let month = months[date.getMonth()];
 day.innerHTML = `${days}, ${data} ${month}`;
+
+function forecastDate(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return days[day];
+}
 function search(city) {
   let apiKey = "85f30c950aad74b0daa3f0ac2c123393";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -53,14 +68,42 @@ navigator.geolocation.getCurrentPosition(showPosition);
 let currentPosition = document.querySelector(".currentPosition");
 currentPosition.addEventListener("click", showPosition);
 
-function currentTemperature(event) {
+function currentCity(event) {
   event.preventDefault();
   let cityInput = document.querySelector(".search").value;
   search(cityInput);
 }
 let locationInput = document.querySelector("#locationInput");
-locationInput.addEventListener("submit", currentTemperature);
+locationInput.addEventListener("submit", currentCity);
 
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<ul class="weatherAfter">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 4) {
+      forecastHTML =
+        forecastHTML +
+        `
+    <li>
+      <span class="weather-forecast-date">${forecastDate(forecastDay.dt)}</span>
+      <span class="weather-forecast-temperature-max">${Math.round(
+        forecastDay.temp.max
+      )}&#176;</span>
+    </li>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</ul>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+function getForecast(corrdinates) {
+  console.log(corrdinates);
+  let apiKey = "445905dadb3d2b0c6f1b916c9d0e3860";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${corrdinates.lat}&lon=${corrdinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 function showTemperature(response) {
   let findName = response.data.name;
   let cityName = document.querySelector("#cityName");
@@ -96,6 +139,9 @@ function showTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
+  iconElement.setAttribute("alt", weatherDescription);
+  getForecast(response.data.coord);
 }
+
+displayForecast();
 search("Stockholm");
